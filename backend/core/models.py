8d7c647d44826b48eff6b7eaf6593991c1d3e12b
Aniket Kumar Ketoghaniya <kumaraniket1188@gmail.com>
2026-02-03 14:38:28 +0530

    Fix NameError by importing User in CreatePostView

--- backend/core/models.py
+++ backend/core/models.py
@@ -1,6 +1,12 @@
-from django.db import models
 from django.contrib.auth.models import User
-from django.db.models import Q
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+from .models import Post
+from .serializers import PostSerializer
+
+
 
 
 class Post(models.Model):
