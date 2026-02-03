3eecf7b4b064a1aec331c0aedd7a1e54a88da5be
Aniket Kumar Ketoghaniya <kumaraniket1188@gmail.com>
2026-02-03 17:28:09 +0530

    Add comments endpoint to URLs

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
-from .views import FeedView, LikePostView, LeaderboardView
-from .views import FeedView, LeaderboardView, CreatePostView
+from .views import FeedView, LikePostView, LeaderboardView, CreatePostView, CreateCommentView
 
 urlpatterns = [
     path("feed/", FeedView.as_view()),
-    path("like/post/<int:post_id>/", LikePostView.as_view()),
-    path("leaderboard/", LeaderboardView.as_view()),
     path("posts/", CreatePostView.as_view()),
-
+    path("like/post/<int:post_id>/", LikePostView.as_view()),
+    path("comments/", CreateCommentView.as_view()),
+    path("leaderboard/", LeaderboardView.as_view()),
 ]
