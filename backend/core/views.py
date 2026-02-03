bbccc3c3453bc7dd8bac1955655d1ae5f0f8f592
Aniket Kumar Ketoghaniya <kumaraniket1188@gmail.com>
2026-02-03 17:09:08 +0530

    fixing posts

--- backend/core/views.py
+++ backend/core/views.py
@@ -2,7 +2,6 @@ from django.contrib.auth.models import User
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework.decorators import api_view, csrf_exempt
 from django.db import transaction
 from django.utils.timezone import now
 from datetime import timedelta
