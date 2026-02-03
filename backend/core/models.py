5d8298f5a4b452b8ad309ef02066cc2e7e406b6c
Aniket Kumar Ketoghaniya <kumaraniket1188@gmail.com>
2026-02-03 14:42:17 +0530

    Fix circular import in models.py

--- backend/core/models.py
+++ backend/core/models.py
@@ -1,10 +1,5 @@
+from django.db import models
 from django.contrib.auth.models import User
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
-
-from .models import Post
-from .serializers import PostSerializer
 
 
 
